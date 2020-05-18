f1564ebb25660ecbf82d565e4e5c69deb80cb6fe
artfulbuster <34212232+artfulbuster@users.noreply.github.com>
2020-05-18 11:32:51 +0100

    Create Task.cs

---
+++ Tasker/Task.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tasker
+{
+    public class Task
+    {
+        int Id;
+        string Title;
+        string Description;
+        int Priority;
+
+    }
+}
