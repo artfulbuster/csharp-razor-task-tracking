5ea8d0e9de74c21df9e4e3201fd6cefac331fe62
artfulbuster <34212232+artfulbuster@users.noreply.github.com>
2020-05-19 11:32:36 +0100

    Update Index.cshtml.cs

--- Tasker/Pages/Index.cshtml.cs
+++ Tasker/Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Permissions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -9,9 +10,15 @@ namespace Tasker.Pages
 {
     public class IndexModel : PageModel
     {
+        public IEnumerable<Task> Tasks { get; set; }
+        private ApplicationDbContext _context;
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public void OnGet()
         {
-
+            Tasks = _context.Tasks.OrderBy(x => x.Priority);
         }
     }
 }
